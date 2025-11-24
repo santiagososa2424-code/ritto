63cf98d97d6519a16602db177c4a10c8e982ddd9
santiagososa2424-code <santiagososa2424@gmail.com>
2025-11-24 17:25:19 -0300

    Update router.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -1,7 +1,5 @@
-import ProtectedRoute from "./components/ProtectedRoute";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// Páginas
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
@@ -13,26 +11,60 @@ import Subscription from "./pages/Subscription";
 import ForgotPassword from "./pages/ForgotPassword";
 import UpdatePassword from "./pages/UpdatePassword";
 
+import ProtectedRoute from "./components/ProtectedRoute";
+
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
+
+        {/* Públicas */}
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/setup" element={<BusinessSetup />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/schedule" element={<Schedule />} />
-
-        {/* Link público de reservas */}
-        <Route path="/booking/:slug" element={<PublicBooking />} />
-
-        {/* Suscripciones */}
-        <Route path="/subscription" element={<Subscription />} />
-
-        {/* Recuperar contraseña */}
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/update-password" element={<UpdatePassword />} />
+        <Route path="/booking/:slug" element={<PublicBooking />} />
+
+        {/* Suscripción */}
+        <Route path="/subscription" element={<Subscription />} />
+
+        {/* Privadas */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/setup"
+          element={
+            <ProtectedRoute>
+              <BusinessSetup />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/services"
+          element={
+            <ProtectedRoute>
+              <Services />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/schedule"
+          element={
+            <ProtectedRoute>
+              <Schedule />
+            </ProtectedRoute>
+          }
+        />
+
       </Routes>
     </BrowserRouter>
   );
