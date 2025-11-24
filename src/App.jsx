a26ac7db492d05e2d5e0f4b5f5ab35e50cccb5f7
santiagososa2424-code <santiagososa2424@gmail.com>
2025-11-24 15:20:03 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      height: "100vh",
+      fontSize: "2rem",
+      fontFamily: "sans-serif"
+    }}>
+      Ritto está funcionando 🚀
+    </div>
+  );
+}
