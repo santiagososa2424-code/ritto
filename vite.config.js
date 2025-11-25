ae06f62a87a720d8da0cb6f3d567ef7abb8d64e2
santiagososa2424-code <santiagososa2424@gmail.com>
2025-11-25 04:08:35 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
-  // IMPORTANTÍSIMO PARA VERCEL
-  root: ".", 
-
-  build: {
-    outDir: "dist",
-    emptyOutDir: true
-  },
-
-  // Para React Router (si más adelante usamos rutas)
-  server: {
-    port: 3000
-  }
 });
