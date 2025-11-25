af96aa36e90a09478e713d80d5e115f113df4946
santiagososa2424-code <santiagososa2424@gmail.com>
2025-11-25 04:13:07 -0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  plugins: [],
 };
