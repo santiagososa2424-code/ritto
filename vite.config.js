5f16f825b7149d48b169f0e3cbe6ba6f053bd3d1
santiagososa2424-code <santiagososa2424@gmail.com>
2026-03-31 14:17:14 -0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  // si tu app usa rutas internas con React Router: opcional base config
-  // base: "./",
-  plugins: [react()]
-});
